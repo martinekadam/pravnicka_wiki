7b2c8f73493b1610a60791956edd8e6cfc2cdf59
Adam Martinek <98785917+martinekadam@users.noreply.github.com>
2025-12-19 23:06:49 +0100

    Drobné úpravy.

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/adam/github/pravnicka_wiki/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/adam/github/pravnicka_wiki/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/adam/github/pravnicka_wiki/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/adam/github/pravnicka_wiki/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/adam/github/pravnicka_wiki/src/css/custom.css"),
+];
---
+++ src/theme/MDXComponents.ts
@@ -0,0 +1,8 @@
+import MDXComponents from '@theme-original/MDXComponents';
+import { Quiz, QuizOption } from '@site/src/components/Quiz';
+
+export default {
+  ...MDXComponents,
+  Quiz,
+  QuizOption,
+};
