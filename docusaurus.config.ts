8e0eb48b9fdd646962a166def2fab70b862084c3
Adam Martinek <98785917+martinekadam@users.noreply.github.com>
2025-12-21 01:14:55 +0100

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://pravnicka.wiki',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'martinekadam', // Usually your GitHub org/user name.
+  projectName: 'pravnicka_wiki', // Usually your repo name.
+  trailingSlash: false, 
 
   onBrokenLinks: 'throw',
 
